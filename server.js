ab1d4dc7de633aef661dceefaf8ee90eaf84ec4f
Becheru888 <becheru93@gmail.com>
2020-03-06 18:20:35 +0000

    express router implemented

--- server.js
+++ server.js
@@ -1,62 +1,15 @@
 require("dotenv").config();
+
 const express = require("express");
 const server = express();
 
+const userRoutes = require("./routes/userRoutes");
+const customersRoutes = require("./routes/customersRoutes");
+
 server.use(express.json());
 
-const DB = require("./db_config/db_helpers");
-
-// USERS END-POINTS
-
-server.get("/", async (req, res) => {
-  try {
-    const users = await DB.allUsers();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(404).json("something went wrong");
-  }
-});
-
-server.post("/post", async (req, res) => {
-  const { username, password } = req.body;
-  try {
-    const users = await DB.addUser(username, password);
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(404).json("something went wrong");
-  }
-});
-
-server.delete("/delete/:id", async (req, res) => {
-  try {
-    const users = await DB.deleteUser(req.params.id);
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(404).json("something went wrong");
-  }
-});
-
-// Customers Endpoints
-
-server.get("/customers", async (req, res) => {
-  try {
-    const cust = await DB.allCustomers();
-    res.status(200).json(cust);
-  } catch (err) {
-    res.status(404).json("Something went wrong");
-  }
-});
-
-server.post("/customers/add", async (req, res) => {
-  const { first_name, last_name, company_name, job_description } = req.body;
-  try {
-    await DB.addCustomer(first_name, last_name, company_name, job_description);
-    const updatedList = await DB.allCustomers();
-    res.status(200).json(updatedList);
-  } catch (err) {
-    res.status(500).json({ message: err });
-  }
-});
+server.use("/users", userRoutes);
+server.use("/customers", customersRoutes);
 
 const port = process.env.PORT || 5000;
 
