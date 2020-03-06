addaf005399c14ebc767c1cb3fd42580ffc960b3
Becheru888 <becheru93@gmail.com>
2020-03-06 17:55:58 +0000

    customers table added

---
+++ migrations/20200306161441_customers.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("customers", tbl => {
+    tbl.increments("id");
+    tbl.string("first_name", 255).defaultTo("N/A");
+    tbl.string("last_name", 255).defaultTo("N/A");
+    tbl.string("company_name", 255).defaultTo("N/A");
+    tbl.string("job_description", 255).defaultTo("N/A");
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("customers");
+};
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ server.use(express.json());
 
 const DB = require("./db_config/db_helpers");
 
+// USERS END-POINTS
+
 server.get("/", async (req, res) => {
   try {
     const users = await DB.allUsers();
@@ -31,6 +33,28 @@ server.delete("/delete/:id", async (req, res) => {
     res.status(200).json(users);
   } catch (err) {
     res.status(404).json("something went wrong");
+  }
+});
+
+// Customers Endpoints
+
+server.get("/customers", async (req, res) => {
+  try {
+    const cust = await DB.allCustomers();
+    res.status(200).json(cust);
+  } catch (err) {
+    res.status(404).json("Something went wrong");
+  }
+});
+
+server.post("/customers/add", async (req, res) => {
+  const { first_name, last_name, company_name, job_description } = req.body;
+  try {
+    await DB.addCustomer(first_name, last_name, company_name, job_description);
+    const updatedList = await DB.allCustomers();
+    res.status(200).json(updatedList);
+  } catch (err) {
+    res.status(500).json({ message: err });
   }
 });
 
