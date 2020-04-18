cc141b741f4d13fabf808e14ab7fd625c79f5da3
Becheru888 <becheru93@gmail.com>
2020-04-18 13:45:57 +0100

    dinamic db connection done

--- knexfile.js
+++ knexfile.js
@@ -5,12 +5,12 @@ module.exports = {
   development: {
     client: "mysql",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: "customers"
-    }
-  }
+      host: process.env.DB_HOST_LOCAL,
+      user: process.env.DB_USER_LOCAL,
+      password: process.env.DB_PASSWORD_LOCAL,
+      database: "customers",
+    },
+  },
 
   // staging: {
   //   client: "postgresql",
@@ -28,19 +28,20 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: "postgresql",
-  //   connection: {
-  //     database: "my_db",
-  //     user: "username",
-  //     password: "password"
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: "knex_migrations"
-  //   }
-  // }
+  production: {
+    client: "mysql",
+    connection: {
+      database: "heroku_325cbd6c4c08876",
+      host: process.env.DB_HOST_PRODUCTION,
+      user: process.env.DB_USERNAME_PRODUCTION,
+      password: process.env.DB_PASS_PRODUCTION,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
 };
