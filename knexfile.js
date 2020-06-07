a826e0cea826ee88005a789bbdfcbc574cdc9250
Becheru888 <becheru93@gmail.com>
2020-06-06 21:47:06 -0700

    .

--- knexfile.js
+++ knexfile.js
@@ -8,7 +8,7 @@ module.exports = {
       host: process.env.DB_HOST_LOCAL,
       user: process.env.DB_USER_LOCAL,
       password: process.env.DB_PASSWORD_LOCAL,
-      database: "customers",
+      database: "cms",
     },
   },
 
